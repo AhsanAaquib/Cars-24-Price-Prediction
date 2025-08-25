01cf8aa2a2c5d640e5ca561d862b14f786c8dde1
AhsanAaquib <ahsanua1994@gmail.com>
2025-08-25 12:06:26 +0530

    Created a Python Sorting Function

---
+++ python_sorting.py
@@ -0,0 +1,19 @@
+def bubble_sort(arr):
+    """
+    Sorts a list using the bubble sort algorithm.
+    Args:
+        arr (list): List of elements to sort.
+    Returns:
+        list: Sorted list.
+    """
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+
+# Example usage:
+if __name__ == "__main__":
+    sample_list = [64, 34, 25, 12, 22, 11, 90]
+    print("Sorted list:", bubble_sort(sample_list))
